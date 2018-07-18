fb465855d60218ce691c1afb6cb5f515b58b62d1
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-18 20:26:20 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,16 @@ class CashRegister
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+    @items = []
   end 
+   def apply_discount
+    if @discount != 0
+      @total = @total - (@discount/100.0 * @total)
+      return "After the discount, the total comes to $#{@total.round}."
+    else
+      return "There is no discount to apply."
+    end
+  end
   
   def total 
     @total 
@@ -14,6 +23,19 @@ class CashRegister
   
   def total=(total)
     @total=total
+  end  
+  
+  def items 
+    @items
   end 
   
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+    quantity.times do 
+      @items << title
+    end 
+  end
+  
+  def void_last_transaction
+  end 
 end
