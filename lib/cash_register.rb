3b6d2497b6fcbae456c8df23c43f6380e13872a8
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-18 17:01:31 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
-
+class CashRegister
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end 
+  
+  def total 
+    @total 
+  end 
+  
+  def discount
+    @discount
+  end
+  
+  def total=(total)
+    @total=total
+  end 
+  
+end
